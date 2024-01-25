2193e2b3d67d26eb9f3b400017e4189ec68b71b8
Danny Armijos <daniel2667@hotmail.es>
2024-01-25 18:23:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Formulario from './components/Formulario.jsx';
+import Header from './components/Header.jsx';
+import ListadoPaciente from "./components/ListadoPaciente.jsx";
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <Formulario/>
+      <ListadoPaciente/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ListadoPaciente.jsx
@@ -0,0 +1,8 @@
+const ListadoPaciente = () =>{
+    return (
+        <div>
+            <h2>Listado Paciente</h2>
+        </div>
+    )
+}
+export default  ListadoPaciente
